e24af84e8ffbf70a0894cf97ffa35450aab1cc69
Sundar <sundar.kishore30@gmail.com>
2024-02-29 15:16:25 +0800

    Create run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Assuming you have a test binary named "test_suite"
+# Change this line according to your actual test command
+./test_suite
+
+# Optionally, you can add more test commands here if needed
+# For example:
+# ./another_test_suite
