1996a279d99f8e67cbac58d27de71319ff7ef900
Sundar <sundar.kishore30@gmail.com>
2024-02-29 15:17:56 +0800

    Update and rename run_test.sh to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 # Assuming you have a test binary named "test_suite"
 # Change this line according to your actual test command
-./test_suite
+./Jenkins
 
 # Optionally, you can add more test commands here if needed
 # For example:
